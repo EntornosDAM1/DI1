3297bc3b5daac67422725f0cb8a9b2f4277daaf9
EntornosDAM1 <hgomezv01@educantabria.es>
2021-12-14 10:29:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-meju8a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-meju8a)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Etiqueta1(){
+  return <div>Prueba</div>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <Etiqueta1/>
+    </div>
+  );
+}
